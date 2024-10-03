2c02e11c3c5d88b4940c6fd72bea3798f485fd39
David_Karnik <david.karnik@tul.cz>
2024-10-03 14:33:10 +0200

    cv03 - úpravy obr

--- cv03/MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.java
+++ cv03/MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapp003objednavka;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        RadioButton button1 = findViewById(R.id.radio1);
+        RadioButton button2 = findViewById(R.id.radio2);
+        RadioButton button3 = findViewById(R.id.radio3);
+
+        ImageView obrazek = findViewById(R.id.imageView);
+
+        button1.setOnClickListener(v -> {
+            obrazek.setImageResource(R.drawable.comp1);
+        });
+
+        button2.setOnClickListener(v -> {
+            obrazek.setImageResource(R.drawable.comp2);
+        });
+
+        button3.setOnClickListener(v -> {
+            obrazek.setImageResource(R.drawable.comp3);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
